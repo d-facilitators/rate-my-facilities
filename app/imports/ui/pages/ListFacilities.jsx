f9e8049db841ad9f2a6836632b558dfb440bd90f
Silvia <silviadb48@gmail.com>
2023-11-14 15:07:01 -1000

    Update Facility Page Mockup

--- app/imports/ui/pages/ListFacilities.jsx
+++ app/imports/ui/pages/ListFacilities.jsx
@@ -1,58 +1,78 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { StarFill, StarHalf, Star } from 'react-bootstrap-icons';
+import React, { useState } from 'react';
+import { Col, Container, Row, Modal, Button } from 'react-bootstrap';
 
 const ListFacilities = () => {
-  // Mockup data for facilities
+  const [showInfo, setShowInfo] = useState(false);
+  const [selectedFacility, setSelectedFacility] = useState(null);
+
   const facilities = [
-    { name: 'Campus Center', reviews: 60, ratings: 4.5 },
-    { name: 'Hamilton Library', reviews: 35, ratings: 3 },
-    { name: 'POST', reviews: 13, ratings: 2.5 },
-    { name: 'Paradise Palms', reviews: 5, ratings: 1.5 },
-    { name: 'Random Place #1', reviews: 10, ratings: 4 },
-    { name: 'Random Place #2', reviews: 20, ratings: 4 },
-    // Add more facilities as needed
+    { name: 'Bilger Hall', imagePath: '/images/bilger-hall.jpg', facilitiesList: [
+      'Restrooms: 2',
+      'Water filters: 1',
+      'Microwaves: 1',
+    ] },
+    { name: 'Campus Center', imagePath: '/images/cc.jpg', facilitiesList: [
+      'Restrooms: 3',
+      'Water filters: 2',
+      'Microwaves: 1',
+    ] },
+    { name: 'Hamilton Library', imagePath: '/images/hamilton-library.jpg', facilitiesList: [
+      'Restrooms: 6',
+      'Water filters: 5',
+      'Microwaves: 0',
+    ] },
+    { name: 'Paradise Palms', imagePath: '/images/paradise-palms.jpg', facilitiesList: [
+      'Restrooms: 3',
+      'Water filters: 2',
+      'Microwaves: 1',
+    ] },
+    { name: 'POST', imagePath: '/images/post.jpg', facilitiesList: [
+      'Restrooms: 3',
+      'Water filters: 2',
+      'Microwaves: 1',
+    ] },
+    { name: 'Shidler College of Business', imagePath: '/images/shidler.jpg', facilitiesList: [
+      'Restrooms: 3',
+      'Water filters: 2',
+      'Microwaves: 1',
+    ] },
   ];
 
-  const renderStars = (rating) => {
-    const fullStars = Math.floor(rating);
-    const hasHalfStar = rating % 1 !== 0;
+  const handleInfoClick = (facility) => {
+    setShowInfo(true);
+    setSelectedFacility(facility);
+  };
 
-    const stars = [];
-    for (let i = 0; i < fullStars; i++) {
-      stars.push(<StarFill key={i} className="text-warning" />);
-    }
-
-    if (hasHalfStar) {
-      stars.push(<StarHalf key={fullStars} className="text-warning" />);
-    }
-
-    const remainingStars = 5 - stars.length;
-    for (let i = 0; i < remainingStars; i++) {
-      stars.push(<Star key={fullStars + i + 1} className="text-warning" />);
-    }
-
-    return stars;
+  const handleCloseInfo = () => {
+    setShowInfo(false);
+    setSelectedFacility(null);
   };
 
   return (
-    <Container className="py-3">
+    <Container className={`py-3 ${showInfo ? 'blur-background' : ''}`}>
       <Row className="justify-content-center">
         <Col>
           <Col className="text-center">
-            <h2>UH Facilities</h2>
+            <h2>UH Manoa Facilities</h2>
           </Col>
-          <Row xs={1} md={2} lg={3} className="g-4">
+          <Row xs={1} md={2} lg={3} className={`g-4 ${showInfo ? 'slide-out' : ''}`}>
             {facilities.map((facility, index) => (
               <Col key={index}>
-                <div className="card">
+                <div className={`card ${showInfo && 'fade-out'}`}>
+                  {/* Use the img tag with the path to the image */}
+                  <img
+                    src={facility.imagePath}
+                    alt={facility.name}
+                    className="card-img-top"
+                    style={{ height: '200px', objectFit: 'cover' }}
+                  />
                   <div className="card-body text-center">
-                    <p> *IMAGE* </p>
                     <h5 className="card-title">{facility.name}</h5>
-                    <p className="card-text">Overall Rating: {facility.reviews}</p>
 
-                    {/* Star icons for ratings */}
-                    <div>{renderStars(facility.ratings)}</div>
+                    {/* Info button */}
+                    <Button variant="success" onClick={() => handleInfoClick(facility)}>
+                      Building Info
+                    </Button>
                   </div>
                 </div>
               </Col>
@@ -60,6 +80,42 @@ const ListFacilities = () => {
           </Row>
         </Col>
       </Row>
+
+      {/* displaying facility information */}
+      <Modal show={showInfo} onHide={handleCloseInfo} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedFacility && selectedFacility.name} Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col xs={12} md={6}>
+                <div>
+                  <img
+                    src={selectedFacility?.imagePath}
+                    alt={selectedFacility?.name}
+                    className="img-fluid"
+                    style={{ height: '300px', objectFit: 'cover' }}
+                  />
+                </div>
+              </Col>
+              <Col xs={12} md={6}>
+                {/* Right column - Facility details */}
+                <ul>
+                  {selectedFacility?.facilitiesList.map((facility, index) => (
+                    <li key={index}>{facility}</li>
+                  ))}
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseInfo}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
